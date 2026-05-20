6d1e25334141854afb33b14bc17aecbb4e3c1236
Liam De Wet <dwliamdw@gmail.com>
2026-05-20 20:34:48 +0200

    Add exercise 2 -Number operations and type conversions

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,23 @@
+//variable called number1 storing a number
+
+const number1 = 67;
+
+//variable called number2 with a different number
+const number2 = 69;
+
+//conver number1 to a string and store in a new variable
+const numb1String = String(number1);
+
+//add number1 and number2, store in a new variable
+const sum = number1 + number2;
+
+//multiply the two numbers
+const product = number1 * number2;
+
+//print all to console
+
+console.log("Number 1: ", number1);
+console.log("Number 2: ", number2);
+console.log("Number 1 in a string format: ", numb1String);
+console.log("Sum: ", sum);
+console.log("Product: ", product);
